a1e877c942fac5af4cab1ccc076b83d50f44cdfc
Lee Wortley <lee.wortley@madetech.com>
2022-04-07 09:55:21 +0100

    feat: adding unit test result parsing to the build job

---
+++ source/gpconnect-user-portal.UnitTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace gpconnect_user_portal.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.Fail();
+    }
+}
