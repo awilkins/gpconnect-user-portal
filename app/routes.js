c43981f1a938b5e3abe40fedd944c9fbe4e2d192
ChristopherJamesMorris <hello@christopherjmorris.com>
2022-04-08 15:23:30 +0100

    Initial baseline

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,17 @@
+var NotifyClient = require('notifications-node-client').NotifyClient,
+    notify = new NotifyClient(process.env.NOTIFYAPIKEY);
+
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+router.post('/email-address-page', function (req, res)
+{
+    notify.sendEmail(
+        '6a5b377e-4763-4618-bd7b-7b31ac823849',
+        req.body.emailAddress,
+        { personalisation: { firstName: req.body.firstName } }
+    );
+    res.redirect('/');
+});
 
 module.exports = router
